2839d8204213dc7e1bcbde2ef6e6974fcd1fc097
BuildTools <unconfigured@null.spigotmc.org>
2022-06-17 17:26:57 +0200

    add messageconfig

--- src/main/java/de/zevyx/kovacore/KovaCore.java
+++ src/main/java/de/zevyx/kovacore/KovaCore.java
@@ -1,15 +1,26 @@
 package de.zevyx.kovacore;
 
+import de.zevyx.kovacore.config.MessageConfig;
+import de.zevyx.kovacore.listener.ChatListener;
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class KovaCore extends JavaPlugin {
 
     private static KovaCore instance;
 
+    private MessageConfig messageConfig;
+
     @Override
     public void onEnable() {
         instance = this;
+        messageConfig = new MessageConfig();
 
+
+        Bukkit.getPluginManager().registerEvents(new ChatListener(), this);
+
+
+        messageConfig.init();
     }
 
     @Override
@@ -19,5 +30,9 @@ public class KovaCore extends JavaPlugin {
 
     public static KovaCore getInstance() {
         return instance;
+    }
+
+    public MessageConfig getMessageConfig() {
+        return messageConfig;
     }
 }
